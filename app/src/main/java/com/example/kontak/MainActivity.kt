5b35a87a1af7626a158b5e7ceebf7a8692bef7a5
ahmadfma <55354990+angguijr@users.noreply.github.com>
2021-06-11 21:23:42 +0800

    tampilkan dialog ketika tombol tambah diklik

--- app/src/main/java/com/example/kontak/MainActivity.kt
+++ app/src/main/java/com/example/kontak/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.kontak
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AlertDialog
+import androidx.lifecycle.ViewModelProvider
+import com.example.kontak.User.UserViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewmodel: UserViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewmodel = ViewModelProvider(this).get(UserViewModel::class.java)
+
+
+        addBtn.setOnClickListener {
+            showDialog()
+        }
+    }
+
+    private fun showDialog() {
+        val dialogView = LayoutInflater.from(this).inflate(R.layout.dialog, null)
+        val builder = this.let {
+            AlertDialog.Builder(it)
+                .setView(dialogView)
+        }
+        val mDialog = builder?.show()
     }
 }
